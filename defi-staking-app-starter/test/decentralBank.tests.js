86679c1ed09f1e948a62b1124e4a659df844f1bd
Alexandre Franca <Alexandre.Franca@bjss.com>
2022-02-07 18:02:05 +0000

    Test failing to compile

--- defi-staking-app-starter/test/decentralBank.tests.js
+++ defi-staking-app-starter/test/decentralBank.tests.js
@@ -1,5 +1,8 @@
 //const { assert } = require('console');
 
+const { tokenToString } = require('typescript');
+const { default: Web3 } = require('web3');
+
 const Tether = artifacts.require('Tether');
 const RWD = artifacts.require('RWD');
 const DecentralBank = artifacts.require('DecentralBank');
@@ -8,11 +11,29 @@ require('chai')
 .use(require('chai-as-promised'))
 .should()
 
-contract('DecentralBank', (account) => {
+contract('DecentralBank', ([owner, customer]) => {
     // Code for testing goes in here
+    let tether, rwd, decentralBank
+    /*
+    function tokens(number) {
+        return Web3.utils.toWei(number,'ether')
+    }
+*/
+    before(async () => {
+        // Load contracts
+        tether = await Tether.new() 
+        rwd = await RWD.new()
+        decentralBank = await DecentralBank.new(rwd.address, tether.address)
+
+        // Transfer all tokens to DecentalBrank (1 million)
+        await rwd.transfer(decentralBank.address, Web3.utils.toWei('1000000','ether'))
+
+        // Transfer 100 mock tethers to Investor
+        await tether.transfer(customer, Web3.utils.toWei('100','ether'), {from: owner})
+    })
+
     describe('Mock Tether Deployment', async () => {
-        it('matches name successfully', async () => {
-            let tether = await Tether.new()
+        it('matches name successfully', async () => {            
             const name = await tether.name()
             assert.equal(name,'Mock Tether Token' )
         })
@@ -20,7 +41,6 @@ contract('DecentralBank', (account) => {
 
     describe('Reward Token', async () => {
         it('matches name successfully', async () => {
-            let rwd = await RWD.new()
             const name = await rwd.name()
             assert.equal(name,'Reward Token' )
         })
