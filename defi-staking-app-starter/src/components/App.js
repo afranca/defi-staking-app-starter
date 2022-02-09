0235773134fc85d58eb5632ef84645222ddc8b0f
Alexandre Franca <Alexandre.Franca@bjss.com>
2022-02-09 00:12:39 +0000

    Hooking up with Metamask

--- defi-staking-app-starter/src/components/App.js
+++ defi-staking-app-starter/src/components/App.js
@@ -1,7 +1,23 @@
 import React, {Component} from "react";
 import Navbar from "./Navbar";
+import Web3 from "web3";
 
 class App extends Component{
+
+    async UNSAFE_componentWillMount(){
+        await this.loadWeb3()
+    }
+
+    async loadWeb3(){
+        if (window.ethereum){
+            window.web3 = new Web3(window.ethereum)
+            await window.ethereum.enable()
+        } else if(window.web3) {
+            window.web3 = new Web3(window.web3.currentProvider)
+        } else {
+            window.alert('No ethereum browser detected! Check out Metamask')
+        }
+    }
 
     constructor(props){
         super(props)
