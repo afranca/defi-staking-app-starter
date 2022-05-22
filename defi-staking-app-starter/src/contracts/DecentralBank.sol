fe4a0ce2163e988df5c3aba38f7c57af4a24e7d6
Alexandre Franca <Alexandre.Franca@bjss.com>
2022-05-22 17:28:42 +0100

    feat: Wiring Tether and RWD in the DecentralBank

--- defi-staking-app-starter/src/contracts/DecentralBank.sol
+++ defi-staking-app-starter/src/contracts/DecentralBank.sol
@@ -1,7 +1,17 @@
 pragma solidity ^0.5.0;
 
+import './RWD.sol';
+import './Tether.sol';
+
 contract DecentralBank {
     address public owner;
     string public name = 'Decentral Bank';
+    RWD public rwd;
+    Tether public tether;
 
+    constructor(RWD _rwd, Tether _tether) public {
+        owner = msg.sender;
+        rwd = _rwd;
+        tether = _tether;
+    }
 }
