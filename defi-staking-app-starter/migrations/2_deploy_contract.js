0f18a3e7e71058f68b3303a6e921f83d4f472fcb
Alexandre Franca <Alexandre.Franca@bjss.com>
2022-05-22 17:30:21 +0100

    feat: Miragtion Script: Wiring Tether and RWD in the DecentralBank - Rewarding 2 account on Ganache with 100 tokens

--- defi-staking-app-starter/migrations/2_deploy_contract.js
+++ defi-staking-app-starter/migrations/2_deploy_contract.js
@@ -1,5 +1,21 @@
 const Tether = artifacts.require('Tether');
+const RWD = artifacts.require('RWD');
+const DecentralBank = artifacts.require('DecentralBank');
 
-module.exports = async function(deployer){
+module.exports = async function(deployer, network, accounts){
    await deployer.deploy(Tether);
+   const tether = await Tether.deployed()
+
+   await deployer.deploy(RWD);
+   const rwd = await RWD.deployed()
+
+   await deployer.deploy(DecentralBank);
+   const decentralBank = await DecentralBank.deployed()
+
+   // Transfer all RWD tokens to Decentral Bank
+   await rwd.transfer(decentralBank.address,'1000000000000000000000000')
+
+   // Distrubute 100 Tether tokens to investor
+   tether.transfer(accounts[1], '100000000000000000000');
+
 };
