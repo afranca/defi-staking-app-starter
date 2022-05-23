87d99adaf32009fb0adb2ee0eee6c7ccb45a53c6
Alexandre Franca <Alexandre.Franca@bjss.com>
2022-05-23 08:45:36 +0100

    fix: folder 'tests' was renamed to 'test'. Truffle looks up a 'test' folder

---
+++ defi-staking-app-starter/test/decentalBank.tests.js
@@ -0,0 +1,19 @@
+const { assert } = require('chai');
+
+const Tether = artifacts.require('Tether');
+const RWD = artifacts.require('RWD');
+const DecentralBank = artifacts.require('DecentralBank');
+
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+
+contract('DecentralBank', (accounts) => {
+    describe('Mock Tether Deployment', async () => {
+        it('matches name successfully', async () => {
+            let tether = await Tether.new()
+            const name = tether.name()
+            assert.equal('Mock Tether Token', name)
+        })
+    })
+})    
