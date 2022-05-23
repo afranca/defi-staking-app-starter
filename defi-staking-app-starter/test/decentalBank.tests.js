fc44cec025c9c82159e139d2e24508b1836bc0e4
Alexandre Franca <Alexandre.Franca@bjss.com>
2022-05-23 11:40:09 +0100

    feat: Create tests for Decentral Bank | Create util function tokens() | Refactor the accounts[] to have better indexes such as Owner & Customer

--- defi-staking-app-starter/test/decentalBank.tests.js
+++ defi-staking-app-starter/test/decentalBank.tests.js
@@ -8,12 +8,24 @@ require('chai')
     .use(require('chai-as-promised'))
     .should()
 
-contract('DecentralBank', (accounts) => {
-   let tether, rwd
+// contract('DecentralBank', (accounts) => {
+contract('DecentralBank', ([owner, customer]) => {
+   let tether, rwd, decentralBank
+
+   function tokens(number) {
+    return web3.utils.toWei(number,'ether')
+}   
 
     before(async ()=>{
         tether = await Tether.new()
         rwd = await RWD.new()
+        decentralBank = await DecentralBank.new(rwd.address, tether.address)
+
+        // Transfer all tokens to DecentalBrank (1 million)
+        await rwd.transfer(decentralBank.address, tokens('1000000'))   
+        
+        // Transfer 100 mock tethers to Investor
+        await tether.transfer(customer, tokens('100'), {from: owner})        
     })
 
 
@@ -34,4 +46,16 @@ contract('DecentralBank', (accounts) => {
             assert.equal(name,'Reward Token' )
         })
     }) 
+
+    describe('DecentralBank Deployment', async () => {
+        it('matches name successfully', async () => {
+            const name = await decentralBank.name()
+            assert.equal('Decentral Bank', name)
+        })
+        it('matches inital Rewards successfully', async () => {
+            const balance = await rwd.balanceOf(decentralBank.address)
+            assert.equal(tokens('1000000'), balance)
+        })    
+         
+    })    
 })    
