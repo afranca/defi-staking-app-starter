2650dd874d23e4c4c07317775aa66f76c682c492
Alexandre Franca <Alexandre.Franca@bjss.com>
2022-05-23 09:29:48 +0100

    refact: Adding a before function to avoid code duplication

--- defi-staking-app-starter/test/decentalBank.tests.js
+++ defi-staking-app-starter/test/decentalBank.tests.js
@@ -9,11 +9,29 @@ require('chai')
     .should()
 
 contract('DecentralBank', (accounts) => {
+   let tether, rwd
+
+    before(async ()=>{
+        tether = await Tether.new()
+        rwd = await RWD.new()
+    })
+
+
     describe('Mock Tether Deployment', async () => {
         it('matches name successfully', async () => {
-            let tether = await Tether.new()
-            const name = tether.name()
+            const name = await tether.name()
             assert.equal('Mock Tether Token', name)
         })
+        it('matches symbol successfully', async () => {
+            const symbol = await tether.symbol()
+            assert.equal('mUSDT', symbol)
+        })        
     })
+
+    describe('Reward Token', async () => {
+        it('matches name successfully', async () => {
+            const name = await rwd.name()
+            assert.equal(name,'Reward Token' )
+        })
+    }) 
 })    
