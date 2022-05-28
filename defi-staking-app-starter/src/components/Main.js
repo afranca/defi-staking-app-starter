8f8ff141b84d7c0a28ae41aa7719bf14e44201d7
Alexandre Franca <Alexandre.Franca@bjss.com>
2022-05-28 09:28:20 +0100

    feat: Main Layout (html/css) done

--- defi-staking-app-starter/src/components/Main.js
+++ defi-staking-app-starter/src/components/Main.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import tetherImg from '../tether.png'
 
 class Main extends Component {
     render() {
@@ -17,7 +18,40 @@ class Main extends Component {
                             <td> RWD</td>
                         </tr>
                     </tbody>
-                </table>             
+                </table>    
+
+                <div className='card mb-2'style={{opacity:'.9'}}>
+                <form                    
+                    className='mb-3'>
+                        <div style={{borderSpacing:'0 1em'}}>
+                            <label className='float-left' style={{marginLeft:'15px'}}><b>Stake Tokens</b></label>
+                            <span className='float-right' style={{marginRight:'8px'}}>
+                                Balance:
+                            </span>
+                            <div className='input-group mb-4'>
+                                <input
+                                
+                                type='text'
+                                placeholder='0'
+                                required />
+                                <div className='input-group-open'>
+                                    <div className='input-group-text'>
+                                        <img src={tetherImg} alt='tether' height='32' />
+                                        &nbsp;&nbsp;&nbsp; USDT
+                                    </div>
+                                </div>
+                            </div>
+                            <button type='submit' className='btn btn-primary btn-lg btn-block'>DEPOSIT</button>
+                        </div>
+                    </form>
+                    <button 
+                    type='submit'
+                    
+                    className='btn btn-primary btn-lg btn-block'>WITHDRAW</button>
+                    <div className='card-body text-center' style={{color:'blue'}}>
+                    AIRDROP 
+                    </div>
+                </div>  
             </div>
         )
     }
