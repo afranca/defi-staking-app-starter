f84e007df4fa0e1722ec7e417992c437196c1c10
Alexandre Franca <Alexandre.Franca@bjss.com>
2022-05-28 09:10:31 +0100

    feat: Creating an empty Main component

--- defi-staking-app-starter/src/components/App.js
+++ defi-staking-app-starter/src/components/App.js
@@ -5,6 +5,7 @@ import Web3 from 'web3';
 import Tether from '../truffle_abis/Tether.json'
 import RWD from '../truffle_abis/RWD.json'
 import DecentralBank from '../truffle_abis/DecentralBank.json'
+import Main from './Main.js'
 
 class App extends Component {
 
@@ -99,7 +100,7 @@ class App extends Component {
           <div className="row">
             <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '600px', minHeight: '100vm'}}>
               <div>
-                Hello World
+                <Main/>
               </div>
             </main>
         </div>
